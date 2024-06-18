436de588c104d0f1ebdd5f01486b919309afc610
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-06-19 01:06:26 +1000

    Express users session

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true, // Mongoose validation (see docs for more)
+    unique: true,
+  },
+  password: {
+    // Most developers would prefer "passwordDigest"
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
